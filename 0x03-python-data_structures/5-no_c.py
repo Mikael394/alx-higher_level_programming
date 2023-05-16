555c48cddb83273721d1ccf4fa7cb754a068d661
Mikael394 <72981523+Mikael394@users.noreply.github.com>
2023-05-16 16:58:51 -0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     for x in l:
         if x == 'c':
             l.remove('c')
-        if x == 'C':
+        elif x == 'C':
             l.remove('C')
     my_string = "".join(l)
     return my_string
